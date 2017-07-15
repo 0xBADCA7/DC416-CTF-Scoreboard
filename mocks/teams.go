38be744e029261bace0b1d7baf0616b2b47e2176
Zack Mullaly <zsck@riseup.net>
2017-07-14 23:27:53 -0400

    Implemented a customizable mock type for the team model

---
+++ mocks/teams.go
@@ -0,0 +1,65 @@
+package mocks
+
+import (
+	"github.com/DC416/DC416-CTF-Scoreboard/models"
+)
+
+// TeamFindFn is the type of a function that effectively satisfies TeamModel.Find
+type TeamFindFn(token string) (models.Team, error)
+
+// TeamAllFn is the type of a function that effectively satisfies TeamModel.All
+type TeamAllFn() ([]models.Team, error)
+
+// TeamSaveFn is the type of a function that effectively satisfies TeamModel.Save
+type TeamSaveFn(*models.Team) error
+
+// TeamUpdateFn is the type of a function that effectively satisfies TeamModel.Update
+type TeamUpdateFn(*models.Team) error
+
+// TeamDeleteFn is the type of a function that effectively satisfies TeamModel.Delete
+type TeamDeleteFn(*models.Team) error
+
+// TeamModelMock implements models.TeamModel in a way that lets us supply custom implementations of each method.
+type TeamModelMock struct {
+	find TeamFindFn
+	all TeamAllFn
+	save TeamSaveFn
+	update TeamUpdateFn
+	delete TeamDeleteFn
+}
+
+// NewTeamModelMock constructs a new mock implementation of models.TeamModel with caller-defined functions.
+func NewTeamModelMock(
+	find TeamFindFn,
+	all TeamAllFn,
+	save TeamSaveFn,
+	update TeamUpdateFn,
+	delete TeamDeleteFn) TeamModelMock {
+	return TeamModelMock{
+		find,
+		all,
+		save,
+		update,
+		delete,
+	}
+}
+
+func (self TeamModelMock) Find(token string) (models.Team, error) {
+	return self.find(token)
+}
+
+func (self TeamModelMock) All() ([]models.Team, error) {
+	return self.all()
+}
+
+func (self TeamModelMock) Save(team *models.Team) error {
+	return self.save(team)
+}
+
+func (self TeamModelMock) Update(team *models.Team) error {
+	return self.update(team)
+}
+
+func (self TeamModelMock) Delete(team *models.Team) error {
+	return self.delete(team)
+}
