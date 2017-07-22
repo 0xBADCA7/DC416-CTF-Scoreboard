004fb7046fee47c29646b50100ee58178349563f
Zack Mullaly <zsck@riseup.net>
2017-07-21 22:53:03 -0400

    Fixed minor errors and switch to assigning inner functions using anonymous functions since the compiler doesn't accept regular inner function declarations

--- endpoints/teams_scoreboard_test.go
+++ endpoints/teams_scoreboard_test.go
@@ -9,8 +9,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/DC416/DC416-CTF-Scoreboard/models"
 	"github.com/DC416/DC416-CTF-Scoreboard/mocks"
+	"github.com/DC416/DC416-CTF-Scoreboard/models"
 )
 
 // Closed over by the TeamModelMock created by NewInMemoryTeamModel.
@@ -24,32 +24,34 @@ type teamState struct {
 // Note that this mock has the quirk that, after a team with an id N gets
 // deleted, all teams with ids m >= n will be decremented to id = m - 1.
 func NewInMemoryTeamModel(state teamState) mocks.TeamModelMock {
-	func find(token string) (models.Team, error) {
+
+	find := func(token string) (models.Team, error) {
 		for _, team := range state.Teams {
 			if team.Token == token {
 				return team, nil
 			}
 		}
-		return models.Team{}, errors.New("Team no found")
-	}
-	
-	func all() ([]models.Team, error) {
-		return state.Teams
+		team := models.Team{}
+		return team, errors.New("Team not found")
 	}
 
-	func save(team *models.Team) error 
+	all := func() ([]models.Team, error) {
+		return state.Teams, nil
+	}
+
+	save := func(team *models.Team) error {
 		team.Id = len(state.Teams)
 		state.Teams = append(state.Teams, *team)
 		return nil
 	}
 
-	func update(team *models.Team) error {
+	update := func(team *models.Team) error {
 		state.Teams[team.Id] = *team
 		return nil
 	}
 
-	func del(team *models.Team) error {
-		state.Teams = append(state.Teams[:team.Id], state.Teams[team.Id+1:]...))
+	del := func(team *models.Team) error {
+		state.Teams = append(state.Teams[:team.Id], state.Teams[team.Id+1:]...)
 		// Wouldn't happen in a DB, but we will do it to make our mock simple.
 		for i := team.Id; i < len(state.Teams); i++ {
 			state.Teams[i].Id -= 1
@@ -57,9 +59,8 @@ func NewInMemoryTeamModel(state teamState) mocks.TeamModelMock {
 		return nil
 	}
 
-	return models.NewTeamModelMock(find, all, save, update, del)
+	return mocks.NewTeamModelMock(find, all, save, update, del)
 }
-
 
 func TestScoreboardEndpoint(test *testing.T) {
 	state := teamState{make([]models.Team, 0)}
@@ -67,13 +68,12 @@ func TestScoreboardEndpoint(test *testing.T) {
 	handler := NewTeamsScoreboardHandler(teamsModel)
 	server := httptest.NewServer(handler)
 	defer server.Close()
-	var err error
 
-	type testFn func (map[string]interface{}) error
+	type testFn func(map[string]interface{}) error
 
 	// Makes a request that gets handled the team scoreboard handler and runs a test function that
 	// can inspect (upon successful decoding) the JSON returned by the handler.
-	func testOutput(runTest testFn) error {
+	testOutput := func(runTest testFn) error {
 		response, err := http.Get(server.URL)
 		if err != nil {
 			return err
@@ -81,7 +81,7 @@ func TestScoreboardEndpoint(test *testing.T) {
 		if response.StatusCode != 200 {
 			return errors.New(fmt.Sprintf("Expected status code 200. Got %d\n", response.StatusCode))
 		}
-		data := map[string]interface{}
+		data := map[string]interface{}{}
 		decoder := json.NewDecoder(response.Body)
 		defer response.Body.Close()
 		err = decoder.Decode(&data)
@@ -92,8 +92,8 @@ func TestScoreboardEndpoint(test *testing.T) {
 	}
 
 	// Produces a function that can be passed to testOutput to check the number of teams returned.
-	func testLenTeams (expectedLen int) testFn {
-		return func (data map[string]interface{}) error {
+	testLenTeams := func(expectedLen int) testFn {
+		return func(data map[string]interface{}) error {
 			teams, found := data["teams"]
 			if !found {
 				return errors.New("Expected response to have a 'teams' field.")
@@ -108,8 +108,8 @@ func TestScoreboardEndpoint(test *testing.T) {
 
 	// Produces a function that can be passed to testOutput that checks that the Nth team
 	// returned has the same fields as a given team.
-	func compareNthTeam(index int, team models.Team) testFn {
-		return func (data map[string]interface{}) error {
+	compareNthTeam := func(index int, team models.Team) testFn {
+		return func(data map[string]interface{}) error {
 			teams := data["teams"]
 			teamFound := teams.([]models.Team)[index]
 			if teamFound != team {
@@ -128,11 +128,11 @@ func TestScoreboardEndpoint(test *testing.T) {
 	// Check that we get the score of the first team that is registered.
 	// Also effectively tests idempotence.
 	team := models.Team{
-		Id: 0,
-		Name: "first",
-		Members: "",
-		Score: 0,
-		SubmitToken: "testtoken",
+		Id:             0,
+		Name:           "first",
+		Members:        "",
+		Score:          0,
+		SubmitToken:    "testtoken",
 		LastSubmission: "never",
 	}
 	teamsModel.Save(&team)
@@ -147,11 +147,11 @@ func TestScoreboardEndpoint(test *testing.T) {
 
 	// Check that inserting another team causes two to be returned.
 	team2 := models.Team{
-		Id: 0,
-		Name: "second",
-		Members: "",
-		Score: 30,
-		SubmitToken: "testtoken2",
+		Id:             0,
+		Name:           "second",
+		Members:        "",
+		Score:          30,
+		SubmitToken:    "testtoken2",
 		LastSubmission: "5 minutes ago",
 	}
 	teamsModel.Save(&team2)
@@ -174,7 +174,6 @@ func TestScoreboardEndpoint(test *testing.T) {
 	if err != nil {
 		test.Error(err)
 	}
-
 
 	// Check that update a team causes its updated info to be returned.
 	team2.Score += 30
