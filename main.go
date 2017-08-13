0cfdaad2b9634373aec609d91400881994b973ab
Zack Mullaly <zsck@riseup.net>
2017-08-13 17:32:23 -0400

    Refactor the old submit handler to serve the submit page

--- main.go
+++ main.go
@@ -39,7 +39,6 @@ func main() {
 	sessions := models.NewSessionModelDB(db)
 	submissions := models.NewSubmissionModelDB(db)
 
-	indexHandler := endpoints.NewIndexHandler(cfg, teams)
 	registrationHandler := endpoints.NewRegistrationHandler(cfg, teams, sessions)
 	adminPageHandler := endpoints.NewAdminPageHandler(cfg, submissions, teams, sessions)
 	deleteTeamHandler := endpoints.NewDeleteTeamHandler(teams, sessions)
@@ -47,8 +46,10 @@ func main() {
 	loginHandler := endpoints.NewLoginHandler(cfg, sessions)
 	logoutHandler := endpoints.NewLogoutHandler(cfg, sessions)
 
+	indexHandler := endpoints.NewIndexHandler(cfg, teams)
 	eventInfoHandler := endpoints.NewEventInfoHandler(cfg.CTFName)
 	scoreboardHandler := endpoints.NewTeamsScoreboardHandler(teams)
+	submitPageHandler := endpoints.NewSubmitPageHandler(cfg)
 	submissionHandler := endpoints.NewTeamSubmitHandler(teams, submissions, cfg.Flags)
 
 	router := mux.NewRouter()
@@ -60,6 +61,7 @@ func main() {
 	router.Handle("/event", eventInfoHandler)
 	router.Handle("/teams/scoreboard", scoreboardHandler)
 	router.Handle("/register", registrationHandler)
+	router.Handle("/submit", submitPageHandler).Methods("GET")
 	router.Handle("/submit", submissionHandler).Methods("POST")
 	router.Handle("/login", loginHandler)
 	router.Handle("/logout", logoutHandler)
