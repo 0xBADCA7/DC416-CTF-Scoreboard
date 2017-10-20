01ca95d5020fcb8f9a2645695ec4d8a7dfb62243
Zack Mullaly <zsck@riseup.net>
2017-10-20 19:01:43 -0400

    Adding messages to the API

--- src/server/resolvers/index.js
+++ src/server/resolvers/index.js
@@ -1,51 +1,67 @@
 const testTeams = [
   {
     rank: 1,
-    name: "Team one",
+    name: 'Team one',
     score: 155,
-    lastSubmission: "5:30 PM"
+    lastSubmission: '5:30 PM',
   },
   {
     rank: 2,
-    name: "DC416",
+    name: 'DC416',
     score: 140,
-    lastSubmission: "4:52 PM"
+    lastSubmission: '4:52 PM',
   },
   {
     rank: 3,
-    name: "h4xx0rz",
+    name: 'h4xx0rz',
     score: 135,
-    lastSubmission: "4:04 PM"
+    lastSubmission: '4:04 PM',
   },
   {
     rank: 4,
-    name: "31337",
+    name: '31337',
     score: 135,
-    lastSubmission: "4:38 PM"
+    lastSubmission: '4:38 PM',
   },
   {
     rank: 5,
-    name: "grep -i flag",
+    name: 'grep -i flag',
     score: 100,
-    lastSubmission: "2:00 PM"
+    lastSubmission: '2:00 PM',
   },
   {
     rank: 6,
-    name: "First place",
+    name: 'First place',
     score: 25,
-    lastSubmission: "1:13 PM"
+    lastSubmission: '1:13 PM',
   },
   {
     rank: 7,
-    name: "Lucky #7",
+    name: 'Lucky #7',
     score: 0,
-    lastSubmission: "No submissions yet"
+    lastSubmission: 'No submissions yet',
+  },
+]
+
+const testMessages = [
+  {
+    posted: 'October 15 at 9:30 AM',
+    content: 'Registrations are open!',
+  },
+  {
+    posted: 'October 18 at 4:30 PM',
+    content: 'We will be posting more information to help you prepare for the CTF soon.',
+  },
+  {
+    posted: 'October 21 at 5:00 PM',
+    content: 'The challenges are all available now! Good luck, everyone!'
   },
 ]
 
 const resolvers = {
   Query: {
     teams: () => testTeams,
+    messages: () => testMessages,
   }
 }
 
